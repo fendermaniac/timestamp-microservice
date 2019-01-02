56280818aec7456bdaf0281f9726bcbdc0e1c8ba
Joe LoPresti <joe@scientiamobile.com>
2019-01-02 17:02:01 -0500

    Build function to handle dates in index.js

--- apps/dateParse.js
+++ apps/dateParse.js
@@ -1,15 +1,25 @@
 import moment from 'moment';
 
-const formats = [
-    'X',
-    'MMMM D, YYYY',
-    'MMMM D YYYY',
-    'MMM D, YYYY',
-    'MMM D YYYY',
-    'D MMMM YYYY',
-    'D MMM YYYY',
-  ];
+const dateParse = (req, res) {
+  let date =  req.params.date;
+  // Check if format is a natural date
+  if (isNaN(date)) {
+  // Check for valid date
+    if (moment(date, 'MMMM DD YYYY').isValid()) {
+      let unix = Math.round((new Date(date).getTime()/1000));
+      let utc = moment(date).format('ddd, DD MMM YYYY HH:mm:ss z');
+      res.send({unix, utc})
+    }
+    else {
+      res.json({'error': 'Invalid Date'});
+    }
 
-const dateParse = moment(date, formats, true);
-
-export default moment;
+  } else {
+    if (moment.unix(date).isValid()){
+      let unix = moment.unix(date);
+      let utc = moment(date).format('ddd, DD MMM YYYY HH:mm:ss z');
+    } else {
+      res.json({'error': 'Invalid Date'});
+    }
+  }
+}
--- index.js
+++ index.js
@@ -1,33 +1,49 @@
 import express from 'express';
+import moment from 'moment';
 
 const app = express();
 const PORT = 3000;
 
+const dateParse = (req, res) => {
+    let date =  req.params.date;
+    // Check if format is a natural date
+    if (isNaN(date)) {
+    // Check for valid date
+      if (moment(date, 'MMMM DD YYYY').isValid()) {
+        let unix = Math.round((new Date(date).getTime()/1000));
+        let utc = moment(date).format('ddd, DD MMM YYYY HH:mm:ss z');
+        res.send({unix, utc})
+      }
+      else {
+        res.json({'error': 'Invalid Date'});
+      }
+  
+    } else {
+      if (moment.unix(date).isValid()){
+        console.log(moment.unix(date)));
+        let unix = moment.unix(date);
+        let utc = moment(date).format('ddd, DD MMM YYYY HH:mm:ss z');
+      } else {
+        res.json({'error': 'Invalid Date'});
+      }
+    }
+  }
+
 app.use("/", express.static("./public"));
 
 app.get('/test', (req,res) => {
-    res.send(dateParse.now)
+
 })
 
 app.get('/api/timestamp/', (req,res) => {
     const now = new Date();
     // const date = Number(Date.now());
-    const unix = now.getTime();
+    const unix = Math.round(now.getTime() / 1000);
     const utc = now.toUTCString();
     res.json({ unix , utc })
 });
 
-app.get('/api/timestamp/:date', (req,res) => {
-    let date = new Date(req.params.date);
-
-    if (req.params.date) {
-      let unix = date.getTime();  
-      let utc = date.toUTCString();
-      res.send({unix, utc}); 
-    } else {
-      res.send({'error': 'Invalid Date'});
-    }
-});
+app.get('/api/timestamp/:date', dateParse);
    
 app.listen(PORT, () => {
     console.log(`Timestamp Microservice is running on port ${PORT}...`);
